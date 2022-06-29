89cca64f82b97d22afbefa93b4e830616be5256a
Brian Gastón di Gangi Pedersen <briandigangi@hotmail.com>
2022-06-29 15:40:51 +0200

    LoanerSite

--- SBO.SBO_Bibliotek.Web/Pages/Loaners/Loaner_Show.cshtml.cs
+++ SBO.SBO_Bibliotek.Web/Pages/Loaners/Loaner_Show.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SBO.SBO_Bibliotek.Services.Models;
+using SBO.SBO_Bibliotek.Services.Services.UserServices;
 
 namespace SBO.SBO_Bibliotek.Web.Pages.Loaners
 {
     public class Loaner_ShowModel : PageModel
     {
+        private readonly IUserService _userService;
+
+        public Loaner_ShowModel (IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public List<LoanerModel> myLoaners { get; set; }
+
         public void OnGet()
         {
+            myLoaners = _userService.GetAllLoaners();
         }
     }
 }
